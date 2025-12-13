bc994a3a9a9be743b88b0e2843016acf2b4fe487
Yorman Romero <139516320+yormanr01@users.noreply.github.com>
2025-12-12 22:43:45 -0500

    test

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import { genkit } from 'genkit';
+import { googleAI } from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY })],
   model: 'googleai/gemini-2.5-flash',
 });
